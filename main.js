524533d2ad8bae1702097d45006db2a03ba72d09
mstovy <micahstovall@Micahs-MacBook-Pro-2.local>
2021-04-03 11:39:22 -0600

    classswork MongoDB

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
-// requirerments 
 const express = require("express"), app = express();
-homeController = require("./controllers/homeController")
-layouts = require("express-ejs-layouts");
-errorController = require("./controllers/errorController")
+homeController = require("./controllers/homeController");
+errorController = require("./controllers/errorController");
+subscribersController = require("./controllers/subscribersController");
+layouts = require("express-ejs-layouts"), mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/confetti_cuisine", {useNewUrlParser: true});
+
 
 app.set("port", process.env.PORT || 3000);
 app.set("view engine", "ejs");
@@ -22,8 +25,12 @@ app.use(express.json());
 
 // routing for website
 app.get("/courses", homeController.showCourses);
-app.get("/contact", homeController.showSignUp);
-app.post("/contact", homeController.postedSignUpForm);
+app.get("/subscribers", subscribersController.getAllSubscribers);
+app.get("/contact", subscribersController.getSubscriptionPage);
+app.post("/subscribe", subscribersController.saveSubscriber);
+
+// app.get("/contact", homeController.showSignUp);
+// app.post("/contact", homeController.postedSignUpForm);
 
 app.use(errorController.pageNotFoundError);
 app.use(errorController.internalServerError);
